a40f9e41ead2358411228478590aee93b8735eff
Sanskar Suryavanshi <ssuryavanshi0001@deloitte.com>
2023-07-31 12:38:21 +0530

    fixed the header highligthing bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,56 +1,44 @@
-import { Component } from "react";
+import React from "react";
 import "./Navbar.css";
-import {Link} from "react-router-dom"
+import { Link, useLocation } from "react-router-dom";
 
-class Navbar extends Component {
-  state = { clicked: false };
-  handleClick = () => {
-    this.setState({ clicked: !this.state.clicked });
-  };
-  render() {
-    return (
-      <>
-        <nav>
-          <a className="logo" href="/">
-            {/* <img src="./images/lgo.svg" />x */}
-          </a>
+const Navbar = () => {
+  // Get the current pathname from useLocation hook
+  const location = useLocation();
+  const currentPath = location.pathname;
 
-          <div>
-            <ul id="navbar" className={this.state.clicked ? "#navbar active" : "#navbar"}>
+  return (
+    <>
+      <nav>
+        <a className="logo" href="/">
+          {/* <img src="./images/lgo.svg" />x */}
+        </a>
 
-              <li>
-                <a className="active" href="/">
-                  Home
-                </a>
-              </li>
-
-
-              <li>
-                <a href="try">Stories</a>
-              </li>
-
-
-              <li>
-                <a href="abt">About</a>
-              </li>
-
-
-              {/* <button className="log" >Login</button> */}
-              <Link className="log" to="/land" >Login</Link>
-
-              
-            </ul>
-          </div>
-          <div id="mobile" onClick={this.handleClick}>
-            <i
-              id="bar"
-              className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
-            ></i>
-          </div>
-        </nav>
-      </>
-    );
-  }
-}
+        <div>
+          <ul id="navbar" className="navbar">
+            <li>
+              <Link className={currentPath === "/" ? "active" : ""} to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className={currentPath === "/try" ? "active" : ""} to="/try">
+                Stories
+              </Link>
+            </li>
+            <li>
+              <Link className={currentPath === "/abt" ? "active" : ""} to="/abt">
+                About
+              </Link>
+            </li>
+            <Link className="log" to="/land">
+              Login
+            </Link>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
 
 export default Navbar;
